b9a8c5daedaa69ddcd591f157331ee9a5fec3704
igor-mlv <malov7704@gmail.com>
2024-07-17 13:52:44 -0400

    added local storage

--- src/app/(todolist)/components/NotesList.tsx
+++ src/app/(todolist)/components/NotesList.tsx
@@ -10,35 +10,56 @@ import { ALL_POSITION, COMPLETE_POSITION, INCOMPLETE_POSITION } from '@/app/stor
 
 function NotesList() {
 
-    const [notesArray, setNotesArray] = React.useState(initialNotesArray);
+    const [notesArrayStorage, setNotesArrayStorage] = React.useState(initialNotesArray);
+    const [displayedNotes, setDisplayedNotes] = React.useState(notesArrayStorage);
     const handleCheckbox = (id: number) => {
-        const updatedNotes = notesArray.map(note => {
+        const updatedNotes = notesArrayStorage.map(note => {
             if (note.id === id) {
                 return { ...note, isCompleted: !note.isCompleted };
             }
             return note;
         });
-        setNotesArray(updatedNotes);
+        setNotesArrayStorage(updatedNotes);
+        setDisplayedNotes(updatedNotes);
     };
+
+    React.useEffect(() => {
+        // Convert array to JSON string
+        const notesArrayJSON = JSON.stringify(notesArrayStorage);
+
+        // Save to local storage
+        localStorage.setItem('notes', notesArrayJSON);
+    }, [notesArrayStorage]);
 
     const position = useSelector((state: RootState) => state.filter.position);
     React.useEffect(() => {
-        if (position === COMPLETE_POSITION) {
-            const updatedNotes = notesArray.filter(note => {
+        if (position === ALL_POSITION) {
+            // Get JSON string from local storage
+            const notesArrayJSON = localStorage.getItem('notes');
+
+            // Check if the notes array exists in local storage
+            if (notesArrayJSON) {
+                // Convert JSON string back to JavaScript array
+                const notes = JSON.parse(notesArrayJSON);
+                setNotesArrayStorage(notes);
+                setDisplayedNotes(notes);
+            }
+        } else if (position === COMPLETE_POSITION) {
+            const updatedNotes = notesArrayStorage.filter(note => {
                 return !note.isCompleted;
             });
-            setNotesArray(updatedNotes);
+            setDisplayedNotes(updatedNotes);
         } else if (position === INCOMPLETE_POSITION) {
-            const updatedNotes = notesArray.filter(note => {
+            const updatedNotes = notesArrayStorage.filter(note => {
                 return note.isCompleted;
             });
-            setNotesArray(updatedNotes);
+            setDisplayedNotes(updatedNotes);
         }
     }, [position]);
 
     return (
         <div className="w-[520px] min-h-[520px] flex flex-col mt-[50px]">
-            {notesArray.map((note: { id: number, content: string, isCompleted: boolean }) => (
+            {displayedNotes.map((note: { id: number, content: string, isCompleted: boolean }) => (
                 <div key={note.id} className="w-full h-[26px] flex flex-row justify-between items-center border-b-[1px] border-primary pb-[32px] mb-[32px]">
                     <Checkbox
                         id={`${note.id}`}
